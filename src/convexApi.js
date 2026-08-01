3e41a75b6829a8f31cfdd205386e015ab469760d
m-sahl <muhammadsahlm06@gmail.com>
2026-08-01 18:16:44 +0530

    Fix white screen crash by replacing node convex/server import with browser-safe Proxy

--- src/convexApi.js
+++ src/convexApi.js
@@ -1,3 +1,10 @@
-import { anyApi } from "convex/server";
-
-export const api = anyApi;
+// Browser-safe Proxy for Convex function references (no Node.js server dependencies)
+export const api = new Proxy({}, {
+  get(_, moduleName) {
+    return new Proxy({}, {
+      get(_, functionName) {
+        return `${String(moduleName)}:${String(functionName)}`;
+      }
+    });
+  }
+});
